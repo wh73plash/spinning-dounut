179bd5d75c4b1b072cc00c3e90bc9a3fdeff3160
JeinKim <wh73plash@gmail.com>
2022-03-26 02:43:35 +0900

    Initial Commit

---
+++ dounut/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  dounut
+//
+//  Created by 김제인 on 2022/03/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
